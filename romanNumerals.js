95fb90549b2de30fcc5468cf3a6c74ffdc0e3c12
and3k <andrewjkasten@gmail.com>
2025-11-08 16:57:15 -0700

    solution to toRomanLazy

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,5 +1,29 @@
+
 function toRomanLazy(num) {
-  return "";
+   let output = ''
+  let romanNumeralToArabic = {
+      I:1,
+      V:5,
+      X:10,
+      L:50,
+      C:100,
+      D:500,
+      M:1000  
+  }
+  let romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+    for (let i = 0; i < romanNumeralPriorityOrder.length; i++){
+
+      let roman = romanNumeralPriorityOrder[i]
+      let arabicValue = romanNumeralToArabic[roman]
+      let number = Math.floor(num / arabicValue)
+
+      for (let i = 0; i < number; i++) {         
+            output += roman    // 
+            num -= arabicValue // 
+        
+          }
+    }
+  return output
 }
 
 function toRoman(num) {
