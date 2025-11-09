4a4b201f0f84488aa1fb643a52476995f28fe937
and3k <andrewjkasten@gmail.com>
2025-11-09 06:31:09 -0700

    solution to both toRomanLazy and toRoman

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,6 +1,7 @@
 
+
 function toRomanLazy(num) {
-   let output = ''
+   
   let romanNumeralToArabic = {
       I:1,
       V:5,
@@ -10,24 +11,65 @@ function toRomanLazy(num) {
       D:500,
       M:1000  
   }
+
   let romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
-    for (let i = 0; i < romanNumeralPriorityOrder.length; i++){
+    
+  let output = ''
+  let lazy = romanNumeralToArabic
+  let order = romanNumeralPriorityOrder
 
-      let roman = romanNumeralPriorityOrder[i]
-      let arabicValue = romanNumeralToArabic[roman]
-      let number = Math.floor(num / arabicValue)
 
-      for (let i = 0; i < number; i++) {         
-            output += roman    // 
-            num -= arabicValue // 
-        
-          }
+  for (let i = 0; i < order.length; i++){
+    let roman = order[i]
+    let arabicValue = lazy[roman]
+    let number = Math.floor(num / arabicValue)
+
+    for (let i = 0; i < number; i++) {         
+      output += roman   
+      num -= arabicValue 
+      }
     }
-  return output
+ return output
 }
 
 function toRoman(num) {
-  return "";
-}
+  let romanNumeralToArabic = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  }
+  
+  //console.log(Object.keys(romanNumeralToArabic))
+  let romanNumeralPriorityOrder = ['M','CM','D','CD','C','XC','L','XL','XL','X','IX','V','IV','I']
 
+  let romanObj = romanNumeralToArabic
+  let orderObj = romanNumeralPriorityOrder
+  let romanOut = ''
+  
+  for (let i = 0; i < orderObj.length; i++) {
+    let roman = orderObj[i];
+    let arabicValue = romanObj[roman];
+    let number = Math.floor(num / arabicValue);
+
+    for (let i = 0; i < number; i++) {
+      romanOut += roman;
+      num -= arabicValue;
+    }
+  }
+ 
+  return romanOut
+}
+console.log(toRoman(4))
+console.log(toRoman(944))
 module.exports = { toRoman, toRomanLazy };
+
